eff31b286fdfbb5d60d2fc8faa0a0448f01d9ca1
Jisargu <jonathnsaa@gmail.com>
2018-05-06 15:30:02 -0500

    Examen JS.  12 preguntas en cards con un botón para mostrar la respuesta

---
+++ js/exam_js.js
@@ -0,0 +1,37 @@
+function toggler1(){
+    document.getElementById("respuesta1").classList.toggle("invisible");
+}
+
+function toggler2(){
+    document.getElementById("respuesta2").classList.toggle("invisible");
+}
+function toggler3(){
+    document.getElementById("respuesta3").classList.toggle("invisible");
+}
+function toggler4(){
+    document.getElementById("respuesta4").classList.toggle("invisible");
+}
+function toggler5(){
+    document.getElementById("respuesta5").classList.toggle("invisible");
+}
+function toggler6(){
+    document.getElementById("respuesta6").classList.toggle("invisible");
+}
+function toggler7(){
+    document.getElementById("respuesta7").classList.toggle("invisible");
+}
+function toggler8(){
+    document.getElementById("respuesta8").classList.toggle("invisible");
+}
+function toggler9(){
+    document.getElementById("respuesta9").classList.toggle("invisible");
+}
+function toggler10(){
+    document.getElementById("respuesta10").classList.toggle("invisible");
+}
+function toggler11(){
+    document.getElementById("respuesta11").classList.toggle("invisible");
+}
+function toggler12(){
+    document.getElementById("respuesta12").classList.toggle("invisible");
+}
